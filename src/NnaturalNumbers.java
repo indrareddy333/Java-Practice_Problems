8621d690fa1de73743401baf904b3b04bb38a6ea
indrareddy333 <reddyindra843@gmail.com>
2024-12-12 07:21:12 +0530

    Practice Problem - To Print N - natural number

---
+++ src/NnaturalNumbers.java
@@ -0,0 +1,7 @@
+public class NnaturalNumbers {
+    public static void main(String[] args) {
+        int num = 10;
+        int result =(num*(num+1) )/2;
+        System.out.println(result);
+    }
+}
