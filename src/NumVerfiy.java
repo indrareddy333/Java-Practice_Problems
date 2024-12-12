d74061e9e45cf89dee7830c854f05cbbaf6a4ea0
indrareddy333 <reddyindra843@gmail.com>
2024-12-12 07:15:22 +0530

    Practice Problem - Whether Entered number positive or negative number

---
+++ src/NumVerfiy.java
@@ -0,0 +1,11 @@
+public class NumVerfiy {
+    public static void main(String[] args) {
+        int num =  1;
+        if(num < 0){
+            System.out.println("Negative Number");
+        }
+        else {
+            System.out.println("Positive Number");
+        }
+    }
+}
