e440a7a752a8ef25a3edd411176610b168b3365d
indrareddy133 <reddyindra843@gmail.com>
2024-12-17 07:26:48 +0530

    Practice Problem - Area of circle

---
+++ src/AreaOfcircle.java
@@ -0,0 +1,10 @@
+public class AreaOfcircle {
+        public static void main(String[] args) {
+            areaOFcircle(43);
+        }
+        public static void areaOFcircle(int r){
+            double area = Math.PI * (r*r);
+            System.out.printf("%.1f",area);
+        }
+
+}
