f0a59cf925065bb0cda0a1bad1cf3343d6a963d5
indrareddy133 <reddyindra843@gmail.com>
2024-12-20 07:19:10 +0530

    line number 5 is corrected

--- src/AreaOfioslationTraingle.java
+++ src/AreaOfioslationTraingle.java
@@ -1,10 +1,10 @@
 public class AreaOfioslationTraingle {
     public static void main(String[] args) {
-        int a = 8;
-        int b = 4;
-        int  tri = ((a*a) - (b*b))/4;
+        int a = 13;
+        int b = 24;
+        int  tri = (a*a) - (b*b)/4;
         double area = Math.sqrt(tri);
         double triangle = (area*b)/2;
-        System.out.printf("%1f",triangle);
+        System.out.println(triangle);
     }
 }
