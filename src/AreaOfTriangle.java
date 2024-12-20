cb862ea1983e6a450233069335c91a1776802577
indrareddy133 <reddyindra843@gmail.com>
2024-12-20 06:54:31 +0530

    Practice Problem - Area of triangle

---
+++ src/AreaOfTriangle.java
@@ -0,0 +1,9 @@
+public class AreaOfTriangle {
+    public static void main(String[] args) {
+        int height = 45;
+        int base = 45;
+        int area = (height*base) / 2;
+        System.out.println(area);
+
+    }
+}
