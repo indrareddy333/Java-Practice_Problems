cccd0762a26fb7986d153b34697874d4846fb417
indrareddy133 <reddyindra843@gmail.com>
2024-12-21 19:22:36 +0530

    Practice Problem - Area of parllelogram

---
+++ src/AreaOFprallelogram.java
@@ -0,0 +1,7 @@
+public class AreaOFprallelogram {
+    public static void main(String[] args) {
+        int base = 3;
+        int height = 4;
+        System.out.println(base*height+" Sq.cm");
+    }
+}
