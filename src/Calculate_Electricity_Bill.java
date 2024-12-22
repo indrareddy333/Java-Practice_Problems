70df5a1e02f7492f1fab3349c1f00052437c330c
indrareddy133 <reddyindra843@gmail.com>
2024-12-22 21:43:03 +0530

    updated code

--- src/Calculate_Electricity_Bill.java
+++ src/Calculate_Electricity_Bill.java
@@ -3,26 +3,31 @@ import java.util.Scanner;
 public class Calculate_Electricity_Bill {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-      application();
+        application();
     }
 
     // user as to enter how many applications they use in the house
     public static void application(){
         Scanner scan = new Scanner(System.in);
         System.out.println("================Welcome ! Calculate your Electricity bill here :)================");
-        System.out.println("Enter the applications of your daily use and get your monthly bill");
-        System.out.println("If your entered all applications Type 'Exit or X' to generate bill");
-        while(true){
-            System.out.print("Enter the application = ");
-            String app = scan.next();
-            //if user want to exit
-            if(app.equals("Exit") || app.equals("exit") || app.equals("X") || app.equals("x")){
-                break;
-            }
-            System.out.print("No. of "+app+"s"+" = ");
-            int NoOFapps = scan.nextInt();
+        System.out.println("Select the applications of your daily use and get your monthly bill");
+        String[] list = {"Air conditioner","Electric water heater", "Light bulb (incandescent)", "Light bulb (LED)","Fan (ceiling, table)", "Air conditioner","refrigerator","Electric cooktop/stove","Microwave","Electric cooker","Television","Desktop computer","washing Machine" };
+        for (int i = 0; i < list.length;i++){
+            System.out.println((i+1) +" . "+ list[i]);
         }
-        System.out.println("Application Exit");
+        for(int i = 0; i < 30;i++){
+            System.out.print("==");
+        }
+        System.out.println();
+        System.out.println("Enter the number form above list = ");
+        switch(scan.nextInt()){
+            case 1:
+                System.out.println(list[0]);
+                System.out.print("Enter the how many hours use in a day : - ");
+                int num = scan.nextInt();
+                int watt = 1200 * num;
+                System.out.println(watt+"kwh");
+        }
     }
 
 }
