94eb39bfa2c75a53501e6f7398484596d0a6386d
indrareddy133 <reddyindra843@gmail.com>
2024-12-26 10:21:37 +0530

    Practice Problem - factors of number

---
+++ src/FactorofNumber.java
@@ -0,0 +1,10 @@
+public class FactorofNumber {
+    public static void main(String[] args) {
+        int num = 24;
+        for (int i = 1; i <= 24; i++){
+            if(num%i==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
