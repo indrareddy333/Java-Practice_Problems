d821f14f8be903bc2ac6b0a874667dd9840d217e
indrareddy133 <reddyindra843@gmail.com>
2024-12-28 22:17:43 +0530

    practice problem - arraylist

---
+++ src/ArraylistPractice.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class ArraylistPractice {
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        array.add(56);
+        array.add(56);
+        array.add(56);array.add(56);
+        array.add(56);
+        array.add(56);
+        System.out.println(array);
+
+
+    }
+}
