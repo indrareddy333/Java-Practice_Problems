f1091debfe7283c5ed838987ad1258b6d2025d00
indrareddy133 <reddyindra843@gmail.com>
2024-12-30 10:53:54 +0530

    Practice Problem - Convert int to string

---
+++ src/ConvertNumToString.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class ConvertNumToString {
+    public static void main(String[] args) {
+        int num = 345;
+        String str = String.valueOf(num);
+        System.out.println(str);
+    }
+}
