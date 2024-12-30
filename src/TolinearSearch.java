fbe3b49edf634eba5671c868dc68fb1103f1e8f6
indrareddy133 <reddyindra843@gmail.com>
2024-12-30 11:44:45 +0530

    Practice Problem - Linear search

--- src/TolinearSearch.java
+++ src/TolinearSearch.java
@@ -1,9 +1,10 @@
 public class TolinearSearch {
     public static void main(String[] args) {
         int[] nums = {2,3,4,5,6,7,8};
-        System.out.println(linearSearch(nums, 2));
+        System.out.println(linearSearch(nums, 8));
     }
     public static int linearSearch(int[] nums, int target){
+        int step = 0;
         for(int i = 0; i < nums.length;i++){
             if(target == nums[i]){
                 return i;
