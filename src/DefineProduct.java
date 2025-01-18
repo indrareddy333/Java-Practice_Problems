c3bb092d0beee28f14d1d5967c7011c875f72d80
indrareddy133 <reddyindra843@gmail.com>
2025-01-18 22:06:13 +0530

    Practice Problem - Define a method that returns the product of two numbers entered by user.

---
+++ src/DefineProduct.java
@@ -0,0 +1,9 @@
+public class DefineProduct {
+    public static void main(String[] args) {
+        //Define a method that returns the product of two numbers entered by user.
+        System.out.println(product(12,2));
+    }
+    public static int product(int p1, int p2){
+        return p1*p2;
+    }
+}
