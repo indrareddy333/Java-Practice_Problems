4889a68726ed8ba07977d458f1efbd26edd51107
indrareddy133 <reddyindra843@gmail.com>
2025-01-25 14:54:38 +0530

    Practice Problem - Selction sort

---
+++ src/Practice_Daily/_06_SelectionSort.java
@@ -0,0 +1,26 @@
+package Practice_Daily;
+
+import java.util.Arrays;
+
+public class _06_SelectionSort {
+    public static void main(String[] args) {
+        int[] nums = {2,1,5,3,3,8,0};
+        selctionSort(nums);
+    }
+    public static void selctionSort(int[] nums){
+        int num  = nums.length-1;
+        while(0 <= num){
+            int max = 0;
+            for(int i = 0; i<=num; i++){
+                if(nums[i]>nums[max]){
+                    max = i;
+                }
+            }
+            int temp = nums[max];
+            nums[max] = nums[num];
+            nums[num] = temp;
+            num--;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
