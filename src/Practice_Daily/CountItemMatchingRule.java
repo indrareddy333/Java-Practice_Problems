d5d6e85150fd145f59868630fa15ee3e4d875c00
indrareddy133 <reddyindra843@gmail.com>
2025-01-27 21:59:10 +0530

    Practice Problem - 1773. Count Items Matching a Rule

---
+++ src/Practice_Daily/CountItemMatchingRule.java
@@ -0,0 +1,33 @@
+package Practice_Daily;
+
+public class CountItemMatchingRule {
+    public static void main(String[] args) {
+        String[][] items = {{"phone","blue","pixel"},{"computer","silver","phone"},{"phone","gold","iphone"}};
+        String ruleKey = "type";
+        String ruleValue = "phone";
+        System.out.println(CountRule(items,ruleKey,ruleValue));
+    }
+    public static int CountRule(String[][] Items, String ruleKey, String ruleValue){
+        int count = 0;
+        for(int i = 0; i < Items.length; i++){
+            if("type".equals(ruleKey)){
+                if(Items[i][0].equals(ruleValue)){
+                    count++;
+                }
+            }
+            else if("color".equals(ruleKey)){
+                if(Items[i][1].equals(ruleValue)){
+                    count++;
+                }
+            }
+            else if("name".equals(ruleKey)){
+                if(Items[i][2].equals(ruleValue)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+}
+
