c87dac923ca01750159b3b023aab9eeae8385d91
indrareddy133 <reddyindra843@gmail.com>
2025-02-02 22:18:21 +0530

    Practice Problem - in Strings

---
+++ src/Practice_Daily/_29_Strings.java
@@ -0,0 +1,18 @@
+package Practice_Daily;
+
+import java.util.Arrays;
+
+public class _29_Strings {
+    public static void main(String[] args) {
+        String name = "String";
+        System.out.println(name.charAt(0));
+        System.out.println("String");
+        System.out.println(89);
+        System.out.println(new int[]{3,2,24});
+        System.out.println(Arrays.toString(new int[]{3,2,24}));
+        String name1 = null;
+        System.out.println(name1);
+
+
+    }
+}
