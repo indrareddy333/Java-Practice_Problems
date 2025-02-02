bd6d5ce2a9ec52a0438a9f5e5df3fdf437b3db87
indrareddy133 <reddyindra843@gmail.com>
2025-02-02 22:26:31 +0530

    Practice Problem - in Strings

---
+++ src/Practice_Daily/PrettyPrinting.java
@@ -0,0 +1,12 @@
+package Practice_Daily;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 432.1245f;
+        System.out.printf("%.2f",a);
+        System.out.println();
+        System.out.printf("%.3f",Math.PI);
+        System.out.println();
+        System.out.printf("hello my name is %s sena reddy", "indra");
+    }
+}
