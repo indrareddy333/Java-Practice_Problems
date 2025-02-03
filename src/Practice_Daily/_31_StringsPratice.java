907cd287551736fcf3d40bae7bd9e718e6248df7
indrareddy133 <reddyindra843@gmail.com>
2025-02-03 23:13:41 +0530

    practice problem for strings

---
+++ src/Practice_Daily/_31_StringsPratice.java
@@ -0,0 +1,15 @@
+package Practice_Daily;
+
+import java.util.ArrayList;
+
+public class _31_StringsPratice {
+    public static void main(String[] args) {
+        System.out.println('a'+'b');
+        System.out.println("a" + "b");
+        System.out.println((char)('a'+ 400));
+        System.out.println("string"+new ArrayList<>());
+        System.out.println("Kunal" + new int[]{45,98});
+        System.out.println("kunal" + new Integer(89));
+        System.out.println(true);
+    }
+}
