74255929126b0785551ed0a637bf382250edf9eb
indrareddy133 <reddyindra843@gmail.com>
2025-02-06 06:59:43 +0530

    Practice problem - patterns

---
+++ src/Practice_Daily/_40_PatternPrinting01.java
@@ -0,0 +1,12 @@
+package Practice_Daily;
+
+public class _40_PatternPrinting01 {
+    public static void main(String[] args) {
+        for(int i = 0; i<5;i++){
+            for(int j = 0; j<5; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
