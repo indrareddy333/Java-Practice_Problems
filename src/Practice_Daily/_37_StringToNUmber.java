b9069f6cf5701fc4515ea46b54caff732bfed271
indrareddy133 <reddyindra843@gmail.com>
2025-02-06 05:55:45 +0530

    Practice problem - String to number convert

---
+++ src/Practice_Daily/_37_StringToNUmber.java
@@ -0,0 +1,15 @@
+package Practice_Daily;
+
+import static java.lang.Integer.MAX_VALUE;
+
+public class _37_StringToNUmber {
+    public static void main(String[] args) {
+        String str = "123";
+        String str1 = "1";
+        int num = Integer.parseInt(str);
+        int num2 = Integer.parseInt(str1);
+        System.out.println(num*num2);
+        int num6 = MAX_VALUE;
+        System.out.println(num6);
+    }
+}
