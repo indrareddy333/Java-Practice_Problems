b1d286c59044417928d35abd07d15ec37618496c
indrareddy133 <reddyindra843@gmail.com>
2025-02-06 07:16:25 +0530

    Practice problem - patterns

--- src/Practice_Daily/_42_PatternPrinting03.java
+++ src/Practice_Daily/_42_PatternPrinting03.java
@@ -3,15 +3,15 @@ package Practice_Daily;
 public class _42_PatternPrinting03 {
     public static void main(String[] args) {
         /*
-         *****
-         ****
-         ***
-         **
-         *
+    1
+    1 2
+    1 2 3
+    1 2 3 4
+    1 2 3 4 5
          */
-        for(int i = 5; i>0;i--){
+        for(int i = 1; i<6;i++){
             for(int j = 0; j < i; j++){
-                System.out.print("# ");
+                System.out.print(j+1+" ");
             }
             System.out.println();
         }
