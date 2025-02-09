3c6e90a63e12438742b9a2dce113624b00158f29
indrareddy133 <reddyindra843@gmail.com>
2025-02-09 15:33:57 +0530

    Practice Problem - Strings

---
+++ src/Practice_Daily/_46_String.java
@@ -0,0 +1,10 @@
+package Practice_Daily;
+public class _46_String {
+    public static void main(String[] args) {
+        String name = "indra";
+        for(int i = 0;i<name.length();i++){
+            char ch = name.charAt(i);
+            System.out.print(ch);
+        }
+    }
+}
