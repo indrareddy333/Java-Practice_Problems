4b46734e7879a2b80a5534a7df8bf92d72f9fc8c
indrareddy133 <reddyindra843@gmail.com>
2025-03-09 20:46:00 +0530

    Practice Problem - Strings

---
+++ src/Practice_Daily/_104_Strings.java
@@ -0,0 +1,12 @@
+package Practice_Daily;
+
+public class _104_Strings {
+    public static void main(String[] args) {
+        String name = "HELLO WORLD";
+        boolean indra = name.contains("HELLO");
+        System.out.println(indra);
+        String hello = "i am good";
+        String world = hello.toUpperCase();
+        System.out.println(world);
+    }
+}
