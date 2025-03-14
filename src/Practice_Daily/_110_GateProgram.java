b154579d8eb9c8b7945f29b3a20cfa5640a8f000
indrareddy133 <reddyindra843@gmail.com>
2025-03-14 06:18:26 +0530

    Problem Practice - Program

---
+++ src/Practice_Daily/_110_GateProgram.java
@@ -0,0 +1,10 @@
+package Practice_Daily;
+
+public class _110_GateProgram {
+    public static void main(String[] args) {
+       int[] nums = {3,4,5,6,7,8,9};
+        for (int i = nums.length-1; i>=0; i--) {
+            System.out.print(nums[i]+" ");
+        }
+    }
+}
