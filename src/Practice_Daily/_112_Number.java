534232df9c6f441081f5b6f3a25bdf6c3f4c32e2
indrareddy133 <reddyindra843@gmail.com>
2025-03-15 21:39:48 +0530

    Problem Practice - Program

---
+++ src/Practice_Daily/_112_Number.java
@@ -0,0 +1,13 @@
+package Practice_Daily;
+
+public class _112_Number {
+    public static void main(String[] args) {
+        int m = 0;
+        int n = 3;
+        int sum1 = 0;
+        int sum = n*(n+1)/2;
+        System.out.println(n-m);
+        System.out.println(m);
+        System.out.println(n);
+    }
+}
