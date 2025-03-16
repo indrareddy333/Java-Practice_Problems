1efc46a0e29497feba6e24aa1c88c97f9b29cec4
indrareddy133 <reddyindra843@gmail.com>
2025-03-16 22:03:14 +0530

    Problem Practice - Program

---
+++ src/Practice_Daily/_113_HcfCode.java
@@ -0,0 +1,13 @@
+package Practice_Daily;
+
+public class _113_HcfCode {
+    public static void main(String[] args) {
+        int num1 = 40, num2 = 60, hcf = 0;
+        for (int i = 1; i <= num1 || i<= num2; i++) {
+            if(num1 % i == 0 && num2 % i == 0){
+                hcf = i;
+            }
+        }
+        System.out.println(hcf);
+    }
+}
